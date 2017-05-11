4025819ddd3c37748fd7d8454b0f8a7a4bc8fb42
Rajesh Somasundaram <fun_rajesh@yahoo.com>
2017-05-11 12:32:06 +0100

    Reformatted

--- chapter-up-and-running/src/main/scala/com/goticks/RestApi.scala
+++ chapter-up-and-running/src/main/scala/com/goticks/RestApi.scala
@@ -3,13 +3,12 @@ package com.goticks
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
-
 import akka.actor._
 import akka.pattern.ask
 import akka.util.Timeout
-
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
 
@@ -27,17 +26,22 @@ trait RestRoutes extends BoxOfficeApi
 
   def routes: Route = eventsRoute ~ eventRoute ~ ticketsRoute
 
-  def eventsRoute =
-    pathPrefix("events") {
+  def eventsRoute = {
+    val directive : Directive[Unit] = pathPrefix("events")
+    val route : server.Route = {
       pathEndOrSingleSlash {
         get {
           // GET /events
-          onSuccess(getEvents()) { events =>
+          val onSuccessMagnet : Future[BoxOffice.Events] = getEvents()
+          val onSuccessDirective : Directive[Tuple1[BoxOffice.Events]] = onSuccess(onSuccessMagnet)
+          onSuccessDirective { events =>
             complete(OK, events)
           }
         }
       }
     }
+    directive (route)
+  }
 
   def eventRoute =
     pathPrefix("events" / Segment) { event =>
